c64e7b1af3b8040d8c9151c04a5537c1853a7ed1
Óscar <alu0100705638@ull.edu.es>
2017-10-21 10:52:29 +0000

    Segunda prueba

--- spec/alimento_spec.rb
+++ spec/alimento_spec.rb
@@ -7,10 +7,16 @@ RSpec.describe Alimento do
   end
 
   before :each do
-    @huevoFrito = Alimentos.new("Huevo Frito")
+    @huevoFrito = Alimentos.new("Huevo Frito", 14.1)
   end
   
   it "Existe un nombre para el alimento" do
     expect(@huevoFrito.nombre).not_to be nil
+    expect(@huevoFrito.nombre).to eq("Huevo Frito")
   end
+  
+  it "Existe la cantidad de proteinas en gramos" do 
+    expect(@huevoFrito.proteinas).not_to be nil
+    expect(@huevoFrito.proteinas).to eq(14.1)
+  end 
 end
