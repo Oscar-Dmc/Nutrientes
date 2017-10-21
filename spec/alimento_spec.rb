b768fd210052ac242e7e9ad8eac36690cf32e0c1
Óscar <alu0100705638@ull.edu.es>
2017-10-21 10:58:56 +0000

    Tercera prueba

--- spec/alimento_spec.rb
+++ spec/alimento_spec.rb
@@ -7,16 +7,19 @@ RSpec.describe Alimento do
   end
 
   before :each do
-    @huevoFrito = Alimentos.new("Huevo Frito", 14.1)
+    @huevoFrito = Alimentos.new("Huevo Frito", 14.1, 0.0)
   end
   
   it "Existe un nombre para el alimento" do
     expect(@huevoFrito.nombre).not_to be nil
-    expect(@huevoFrito.nombre).to eq("Huevo Frito")
   end
   
   it "Existe la cantidad de proteinas en gramos" do 
     expect(@huevoFrito.proteinas).not_to be nil
-    expect(@huevoFrito.proteinas).to eq(14.1)
-  end 
+  end
+  
+  it "Existe la cantidad de glucidos del alimento en gramos" do
+    expect(@huevoFrito.glucidos).not_to be nil
+  end
+  
 end
