fd4f016008b380e9e3bd585055e4c98001a8d9c0
Óscar <alu0100705638@ull.edu.es>
2017-10-21 10:59:59 +0000

    Generando codigo para satisfacer la tercera prueba

--- lib/alimento/alimento.rb
+++ lib/alimento/alimento.rb
@@ -1,11 +1,12 @@
 
 
 class Alimentos
-    attr_accessor :nombre, :proteinas
+    attr_accessor :nombre, :proteinas, :glucidos
     
-    def initialize(nombreA, proteinasA)
+    def initialize(nombreA, proteinasA, glucidosA)
         @nombre = nombreA
         @proteinas = proteinasA
+        @glucidos = glucidosA
     end
 
 end 
