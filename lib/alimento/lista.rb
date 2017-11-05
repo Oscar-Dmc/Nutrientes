ca4b487ead4685635005a3a42db5383c68ed49b6
Óscar <alu0100705638@ull.edu.es>
2017-11-05 12:38:04 +0000

    Implementamos el código para satisfacer la prueba anterior

--- lib/alimento/lista.rb
+++ lib/alimento/lista.rb
@@ -8,4 +8,27 @@ class Lista
         @cola = nil
         @size = 0
     end 
+    
+    def insertar_cabeza(nodo)
+        if vacia
+            nodo[:siguiente] = nil
+            nodo[:anterior] = nil
+            @cabeza = nodo
+            @cola = nodo
+        else
+            nodo[:siguiente] = @cabeza
+            @head[:anterior] = nodo
+            nodo[:anterior] = nil
+            @cabeza = nodo
+        end
+        @size = @size + 1 
+    end
+    
+    def vacia
+        if size==0
+            true
+        else 
+            false
+        end
+    end 
 end 
