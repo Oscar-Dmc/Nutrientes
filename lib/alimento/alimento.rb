56b86b0e1576e34039d60e58b2cac9642fccc730
Óscar <alu0100705638@ull.edu.es>
2017-11-06 10:49:28 +0000

    Modificamos la clase Alimentos para la herencia

--- lib/alimento/alimento.rb
+++ lib/alimento/alimento.rb
@@ -1,23 +1,22 @@
 
-
+#Clase padre de alimentos
 class Alimentos
-    attr_accessor :nombre, :proteinas, :glucidos, :lipidos
+    attr_accessor :proteinas, :glucidos, :lipidos
     
-    def initialize(nombreA, proteinasA, glucidosA, lipidosA)
-        @nombre = nombreA
+    def initialize(proteinasA, glucidosA, lipidosA)
         @proteinas = proteinasA
         @glucidos = glucidosA
         @lipidos = lipidosA
     end
     
     def to_s
-        s = "#{@nombre}\t#{@proteinas}\t#{@glucidos}\t#{@lipidos}"
-        
+        s = "#{@proteinas}\t#{@glucidos}\t#{@lipidos}"
+        s
     end
     
     def v_energetico
         valor = (@proteinas * 4) + (@glucidos * 4) + (@lipidos * 9)
-        
+        valor
     end 
 
 end 
